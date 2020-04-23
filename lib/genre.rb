79e0315b52d896b4e1691deab3e3cd928402365a
Omar Nabi <onabi20@gmail.com>
2020-04-23 05:09:33 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,26 @@
+class Genre
+  attr_accessor :name, :genre
+  @@all = Array.new
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def songs
+    Song.all.select do |song|
+      song.genre == self
+    end
+  end
+  
+  def artists
+    Song.all.map do |song|
+      song.artist
+    end
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  end
